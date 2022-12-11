3c4631a0d09714201382399221bbe630b7d597c9
Engin Gülek <engingulek0@gmail.com>
2022-12-11 14:35:09 +0300

    Category - Get and post fetch APIs of the category died  written

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
+const categoriesRouter = require("./routers/categories")
+app.use(bodyParser.json())
+
+mongoose.connect("mongodb+srv://cookApp:C9TIRlaZq4NPNv3b@engingulek.hscve.mongodb.net/cookAppDatabase?retryWrites=true&w=majority")
+mongoose.connection.once("open",()=>{
+    console.log("Connect to DB!")
+
+}).on("error",(error)=>{
+    console.log("Failed to connect" + error)
+})
+
+app.use("/",categoriesRouter)
 
 app.listen(3000,()=> { 
     console.log("Server is running on port 8080")
---
+++ routers/categories.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Category = require('../models/Category');
+const router = express.Router();
+
+router.get("/categories",(req,res)=>{
+    Category.find()
+    .then(categories => {
+        res.json({
+            categories:categories,
+            success:1
+        })
+        
+    }).catch(err=>{
+        res.json(err)
+    })
+})
+
+router.post("/addCategory",(req,res)=>{
+   
+    const newCategory = new Category({
+        categoryName : req.body.categoryName
+    })
+    newCategory.save()
+    res.json({
+        success:1,
+        message:"success"
+    })
+    
+})
+
+module.exports = router
